64919bdc635325505becb8889897b1e35ddfa70f
Bernard Xiong <bernard.xiong@gmail.com>
2017-08-16 10:27:39 +0800

    Add User Components Initialization.

---
+++ components/user_com.c
@@ -0,0 +1,8 @@
+#include <rtthread.h>
+
+#include "user_com.h"
+
+WEAK int user_components_init(void)
+{
+    return 0;
+}
---
+++ components/user_com.h
@@ -0,0 +1,6 @@
+#ifndef USER_COMPONENTS_H__
+#define USER_COMPONENTS_H__
+
+int user_components_init(void);
+
+#endif
