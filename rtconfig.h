a8504d523214ee872ae480106e31cd1db138b74f
Bernard Xiong <bernard.xiong@gmail.com>
2017-08-25 15:29:36 +0800

    Update config

--- rtconfig.h
+++ rtconfig.h
@@ -12,8 +12,8 @@
 #define RT_TICK_PER_SECOND 100
 #define RT_DEBUG
 #define RT_USING_OVERFLOW_CHECK
-/* CONFIG_RT_DEBUG_INIT is not set */
-/* CONFIG_RT_DEBUG_THREAD is not set */
+/* RT_DEBUG_INIT is not set */
+/* RT_DEBUG_THREAD is not set */
 #define RT_USING_HOOK
 #define RT_USING_IDLE_HOOK
 #define RT_USING_TIMER_SOFT
@@ -35,7 +35,7 @@
 #define RT_USING_MEMHEAP
 #define RT_USING_HEAP
 #define RT_USING_SMALL_MEM
-/* CONFIG_RT_USING_SLAB is not set */
+/* RT_USING_SLAB is not set */
 
 /* Kernel Device Object */
 
@@ -43,15 +43,15 @@
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 128
 #define RT_CONSOLE_DEVICE_NAME "uart"
-/* CONFIG_RT_USING_MODULE is not set */
+/* RT_USING_MODULE is not set */
 
 /* RT-Thread Components */
 
-/* CONFIG_RT_USING_COMPONENTS_INIT is not set */
+/* RT_USING_COMPONENTS_INIT is not set */
 
 /* C++ features */
 
-/* CONFIG_RT_USING_CPLUSPLUS is not set */
+/* RT_USING_CPLUSPLUS is not set */
 
 /* Command shell */
 
@@ -59,13 +59,13 @@
 #define FINSH_USING_SYMTAB
 #define FINSH_USING_DESCRIPTION
 #define FINSH_THREAD_STACK_SIZE 4096
-/* CONFIG_FINSH_USING_AUTH is not set */
+/* FINSH_USING_AUTH is not set */
 #define FINSH_DEFAULT_PASSWORD "rtthread"
-#define FINSH_CMD_SIZE 80
+#define FINSH_CMD_SIZE 256
 #define FINSH_THREAD_PRIORITY 20
 #define FINSH_USING_MSH
 #define FINSH_USING_MSH_DEFAULT
-/* CONFIG_FINSH_USING_MSH_ONLY is not set */
+/* FINSH_USING_MSH_ONLY is not set */
 
 /* Device virtual file system */
 
@@ -75,38 +75,38 @@
 #define DFS_FD_MAX 4
 #define RT_USING_DFS_ELMFAT
 #define RT_DFS_ELM_CODE_PAGE 437
-/* CONFIG_ELM_LFN_MODE0 is not set */
-/* CONFIG_ELM_LFN_MODE1 is not set */
-/* CONFIG_ELM_LFN_MODE2 is not set */
+/* ELM_LFN_MODE0 is not set */
+/* ELM_LFN_MODE1 is not set */
+/* ELM_LFN_MODE2 is not set */
 #define ELM_LFN_MODE3
 #define RT_DFS_ELM_USE_LFN 3
 #define RT_DFS_ELM_MAX_LFN 255
 #define RT_DFS_ELM_DRIVES 2
 #define RT_DFS_ELM_MAX_SECTOR_SIZE 512
-/* CONFIG_RT_DFS_ELM_USE_ERASE is not set */
+/* RT_DFS_ELM_USE_ERASE is not set */
 #define RT_DFS_ELM_REENTRANT
 #define RT_USING_DFS_DEVFS
-/* CONFIG_RT_USING_DFS_NET is not set */
+/* RT_USING_DFS_NET is not set */
 #define RT_USING_DFS_ROMFS
-/* CONFIG_RT_USING_DFS_NFS is not set */
+/* RT_USING_DFS_NFS is not set */
 
 /* Device Drivers */
 
 #define RT_USING_DEVICE_IPC
 #define RT_USING_SERIAL
-/* CONFIG_RT_USING_CAN is not set */
-/* CONFIG_RT_USING_HWTIMER is not set */
+/* RT_USING_CAN is not set */
+/* RT_USING_HWTIMER is not set */
 #define RT_USING_I2C
 #define RT_USING_I2C_BITOPS
 #define RT_USING_PIN
-/* CONFIG_RT_USING_MTD_NOR is not set */
-/* CONFIG_RT_USING_MTD_NAND is not set */
-/* CONFIG_RT_USING_RTC is not set */
-/* CONFIG_RT_USING_SDIO is not set */
-/* CONFIG_RT_USING_SPI is not set */
-/* CONFIG_RT_USING_WDT is not set */
-/* CONFIG_RT_USING_USB_HOST is not set */
-/* CONFIG_RT_USING_USB_DEVICE is not set */
+/* RT_USING_MTD_NOR is not set */
+/* RT_USING_MTD_NAND is not set */
+/* RT_USING_RTC is not set */
+/* RT_USING_SDIO is not set */
+/* RT_USING_SPI is not set */
+/* RT_USING_WDT is not set */
+/* RT_USING_USB_HOST is not set */
+/* RT_USING_USB_DEVICE is not set */
 
 /* libc */
 
@@ -118,13 +118,13 @@
 /* light weight TCP/IP stack */
 
 #define RT_USING_LWIP
-/* CONFIG_RT_USING_LWIP141 is not set */
-/* CONFIG_RT_USING_LWIP200 is not set */
+/* RT_USING_LWIP141 is not set */
+/* RT_USING_LWIP200 is not set */
 #define RT_USING_LWIP202
 #define RT_LWIP_IPV6
 #define RT_LWIP_IGMP
 #define RT_LWIP_ICMP
-/* CONFIG_RT_LWIP_SNMP is not set */
+/* RT_LWIP_SNMP is not set */
 #define RT_LWIP_DNS
 #define RT_LWIP_DHCP
 #define IP_SOF_BROADCAST 1
@@ -132,8 +132,9 @@
 #define LWIP_USING_DHCPD
 #define RT_LWIP_UDP
 #define RT_LWIP_TCP
-/* CONFIG_RT_LWIP_PPP is not set */
+/* RT_LWIP_PPP is not set */
 #define RT_LWIP_PBUF_NUM 16
+#define RT_LWIP_NETCONN_NUM 8
 #define RT_LWIP_RAW_PCB_NUM 4
 #define RT_LWIP_UDP_PCB_NUM 4
 #define RT_LWIP_TCP_PCB_NUM 4
@@ -156,7 +157,7 @@
 
 /* Modbus master and slave stack */
 
-/* CONFIG_RT_USING_MODBUS is not set */
+/* RT_USING_MODBUS is not set */
 
 /* RT-Thread online packages */
 
@@ -166,7 +167,7 @@
 
 /* miscellaneous packages */
 
-/* CONFIG_PKG_USING_HELLO is not set */
+/* PKG_USING_HELLO is not set */
 
 /* multimedia packages */
 
@@ -180,11 +181,19 @@
 #define RT_ESP_PSRAM_SIZE 4096
 #define CONFIG_ESP_AUDIO
 #define CONFIG_CHIP_ESP32
-/* CONFIG_CONFIG_BOARD_ESP32 is not set */
+/* CONFIG_BOARD_ESP32 is not set */
 #define CONFIG_BOARD_RTIOT_AUDIO
-/* CONFIG_CONFIG_BOARD_LYRAT is not set */
+/* CONFIG_BOARD_LYRAT is not set */
 #define RT_USING_WIFI
-/* CONFIG_IDF_MBEDTLS is not set */
+
+/* Multi-Media Audio */
+
+#define RT_USING_AUDIO_PLAYER
+
+/* components in IDF of Espressif */
+
+/* IDF_MBEDTLS is not set */
+/* IDF_LWIP is not set */
 #include "rtconfig_project.h"
 
 #endif
