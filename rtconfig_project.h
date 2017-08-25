8edcf636830037c6638ee0843b90dd6fd2d493d3
Bernard Xiong <bernard.xiong@gmail.com>
2017-08-25 14:48:05 +0800

    Keep more compatibility with lwip in IDF.

--- rtconfig_project.h
+++ rtconfig_project.h
@@ -1,6 +1,11 @@
 #ifndef RTCONFIG_PROJECT_H__
 #define RTCONFIG_PROJECT_H__
 
+#ifdef IDF_LWIP
+#undef LWIP_SO_RCVBUF
+#undef SO_REUSE
+#endif
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
@@ -13,7 +18,7 @@
 #undef RT_TICK_PER_SECOND
 #endif
 
-#define RT_TICK_PER_SECOND	CONFIG_FREERTOS_HZ
+#define RT_TICK_PER_SECOND  CONFIG_FREERTOS_HZ
 
 #endif
 
